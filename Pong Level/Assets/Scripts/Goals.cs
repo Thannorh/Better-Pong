e0cecd792a522648cd8093eb4982639877a6562d
Thannorh <hunter.powrie@digipen.edu>
2019-11-07 09:06:34 -0800

    Ok Boomer

---
+++ Pong Level/Assets/Scripts/Goals.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckForImpact()
+    {
+        //if Something Hits the Goal...
+
+        //Ball Resets to Starting Position (Ballstart Value goes to zero)
+
+        //Increase Player Score
+
+        
+    }
+}
--- Pong Level/Assets/Scripts/ballstart.cs
+++ Pong Level/Assets/Scripts/ballstart.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ballstart : MonoBehaviour
 {
+    public int ballactive = 0; 
     public float XSpeed = 0.0f;
     public float YSpeed = 0.0f;
     public Vector2 StartingPosition = new Vector3(0f, -4.1f);
     // Start is called before the first frame update
     void Start()
     {
+        ballactive = 0; 
         transform.position = (Vector2)(StartingPosition);
     }
 
@@ -21,9 +23,11 @@ public class ballstart : MonoBehaviour
 
     void StartingPush()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKey(KeyCode.Space) && ballactive == 0)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(XSpeed, YSpeed));
+            ballactive++;
+
         }
     }
 }
